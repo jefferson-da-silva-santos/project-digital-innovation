3f91c079283af61d9235cb9980a3c0a50ed9028d
jefferson-da-silva-santos <jeffrrWPG678@gmail.com>
2024-09-03 18:13:28 -0300

    Lógica do menu mobile criada

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
+const btnMenu = document.querySelector('.group-menu');
+const listMenu = document.querySelector('.list-menus');
+let menuVisible = false;
+
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('btn-login').addEventListener('click', (event) => {
     event.preventDefault();
@@ -10,4 +14,36 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.group-option-login').style.display = 'none';
   });
 
+  btnMenu.addEventListener('click', () => {
+    if (!menuVisible) {
+      openMenu(listMenu);
+    } else {
+      closeMenu(listMenu);
+    }
+  });
+
+  document.querySelectorAll('.item-menu').forEach(element => {
+    element.addEventListener('click', () => {
+      closeMenu(listMenu);
+    })
+  })
+
 });
+
+function openMenu(menu) {
+  menu.style.display = 'flex';
+  document.body.style.overflow = 'hidden';
+  document.querySelectorAll('.linha').forEach(element => {
+    element.style.backgroundColor = '#1695fe';
+  });
+  menuVisible = !menuVisible;
+}
+
+function closeMenu(menu) {
+  menu.style.display = 'none';
+  document.body.style.overflow = 'auto';
+  document.querySelectorAll('.linha').forEach(element => {
+    element.style.backgroundColor = 'white';
+  });
+  menuVisible = !menuVisible;
+}
